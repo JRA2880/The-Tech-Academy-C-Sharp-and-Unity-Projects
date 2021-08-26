f364a4c4fa751f28cfdd2d74b03f3b7412ad6a00
JRA2880 <johnrusselladamsjr@gmail.com>
2021-08-26 17:26:33 -0400

    Submission of While and Do While Assignment
    Submission of While and Do While Assignment to GitHub

---
+++ DoWhileConsoleApp/DoWhileConsoleApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhileConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Guess an animal?:");
+            string animal = Console.ReadLine();
+            bool isGuessed = animal == "tiger";
+
+            do
+            {
+                switch (animal)
+                {
+                    case "walrus":
+                        Console.WriteLine("You guessed walrus. Try again.");
+                        Console.WriteLine("Guess an animal?");
+                        animal = Console.ReadLine();
+                        break;
+                    case "dolphin":
+                        Console.WriteLine("You guessed dolphin. Try again.");
+                        Console.WriteLine("Guess an animal?");
+                        animal = Console.ReadLine();
+                        break;
+                    case "penguin":
+                        Console.WriteLine("You guessed penguin. Try again.");
+                        Console.WriteLine("Guess an animal?");
+                        animal = Console.ReadLine();
+                        break;
+                    case "tiger":
+                        Console.WriteLine("You guessed tiger. That is correct!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong!");
+                        Console.WriteLine("Guess an animal?");
+                        animal = Console.ReadLine();
+                        break;
+                }
+            }
+            while (!isGuessed);
+            Console.Read();
+        }
+    }
+}
---
+++ WhileConsoleApp/WhileConsoleApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Guess an animal?:");
+            string animal = Console.ReadLine();
+            bool isGuessed = animal == "tiger";
+
+            while(!isGuessed)
+            {
+                switch (animal)
+                {
+                    case "walrus":
+                        Console.WriteLine("You guessed walrus. Try again.");
+                        Console.WriteLine("Guess an animal?");
+                        animal = Console.ReadLine();
+                        break;
+                    case "dolphin":
+                        Console.WriteLine("You guessed dolphin. Try again.");
+                        Console.WriteLine("Guess an animal?");
+                        animal = Console.ReadLine();
+                        break;
+                    case "penguin":
+                        Console.WriteLine("You guessed penguin. Try again.");
+                        Console.WriteLine("Guess an animal?");
+                        animal = Console.ReadLine();
+                        break;
+                    case "tiger":
+                        Console.WriteLine("You guessed tiger. That is correct!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong!");
+                        Console.WriteLine("Guess an animal?");
+                        animal = Console.ReadLine();
+                        break;
+                }
+            }
+            Console.Read();
+        }
+    }
+}
