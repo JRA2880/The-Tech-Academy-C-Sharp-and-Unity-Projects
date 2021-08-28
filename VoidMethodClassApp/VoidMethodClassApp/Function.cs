2ee1204f41c1ce6da751b5df74418911039fcb34
JRA2880 <johnrusselladamsjr@gmail.com>
2021-08-28 19:48:03 -0400

    Submission of Void Method Class Application
    Submission of Void Method Application

---
+++ VoidMethodClassApp/VoidMethodClassApp/Function.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodClassApp
+{
+    class Function
+    { 
+        public void FunctionHelper(int num1, int num2)
+        {
+            int result = num1 + num1;
+            Console.WriteLine("Second Number: {0}",num2);
+        }
+    }
+}
---
+++ VoidMethodClassApp/VoidMethodClassApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Developer: John R Adams
+/// Task: Method Class Submission Assignment
+/// Requirements: 
+/// 1.) Create a class. In that class, create a void method that takes two integers as parameters. 
+///     Have the method do a math operation on the first integer and display the second integer to the screen.
+/// 2.) In the Main() method of the console app, instantiate the class.
+/// 3.) Call the method in the class, passing in two numbers.
+/// 4.) Call the method in the class, specifying the parameters by name.
+/// </summary>
+
+namespace VoidMethodClassApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Function function = new Function();
+            function.FunctionHelper(num1: 5, num2: 10);
+            Console.Read();
+        }
+    }
+}
