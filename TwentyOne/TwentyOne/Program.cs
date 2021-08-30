3463e73fc65b940a898b3a660eeedf9176c4586c
JRA2880 <johnrusselladamsjr@gmail.com>
2021-08-30 01:50:30 -0400

    Update of TwentyOne Project

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,28 +10,27 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            //Deck deck = new Deck();
-            List<int> numberList = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
-            //int sum = numberList.Sum();
-            int sum = numberList.Where(x => x > 2).Sum();
-            Console.WriteLine(sum);
-
-            //List<Card> newList = deck.Cards.Where(x => x.Face == Face.King).ToList();
-            //foreach (Card card in newList)
-            //{
-            //    Console.WriteLine(card.Face);
-            //} 
-            //int count = deck.Cards.Count(x => x.Face == Face.Ace);
-            //Console.WriteLine(count);
-            //deck.Shuffle(2);
-
-            //foreach (Card card in deck.Cards)
-            //{
-            //    Console.WriteLine(card.Face + " of " + card.Suit);
-            //}
-            //Console.WriteLine(deck.Cards.Count);
-            Console.WriteLine("This line of code got it.");
-            Console.ReadLine();
+            Console.WriteLine("Welcome to the Grand Hotel and Casino.  Let's start by telling me your name.");
+            string playerName = Console.ReadLine();
+            Console.WriteLine("And how much money did you bring today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
+            string answer = Console.ReadLine().ToLower();
+            if(answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya" || answer == "yeap")
+            {
+                Player player = new Player(playerName,bank);
+                Game game = new TwentyOneGame();
+                game += player;
+                player.isActivelyPlaying = true;
+                while(player.isActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thank you for playing!");
+            }
+            Console.WriteLine("Feel free to look around the casino. Bye for now.");
+            Console.Read();
         }    
     }
 }
